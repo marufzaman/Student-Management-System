369f6984a6774a51998745896823e2e7a2d031a8
A. M. Almarufuzzaman <sajalmaruf@gmail.com>
2022-02-11 00:38:55 +0600

    Student-Profile

---
+++ student/src/main/java/com/management/student/profile/StudentProfileRepository.java
@@ -0,0 +1,20 @@
+package com.management.student.profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {
+
+    @Query("SELECT s FROM StudentProfile s " +
+            "WHERE s.name = :name and s.gender = :gender")
+    Optional<StudentProfile> findStudentProfile(
+            @Param("name") String name,
+            @Param("gender") String gender
+    );
+
+}
