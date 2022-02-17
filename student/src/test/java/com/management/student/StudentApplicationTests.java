772c0fa47b441bba8999efa0f68b0fc1189c73a2
Tausif abir <tausifabir94@gmail.com>
2022-02-17 16:15:10 +0600

    course add api  validation

--- student/src/test/java/com/management/student/StudentApplicationTests.java
+++ student/src/test/java/com/management/student/StudentApplicationTests.java
@@ -3,11 +3,36 @@ package com.management.student;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 @SpringBootTest
 class StudentApplicationTests {
 
+
+	Calculate calculate = new Calculate();
 	@Test
 	void contextLoads() {
+
+		int numberOne =30;
+		int numberTwo = 20;
+
+		int expected =50;
+
+		int result = calculate.add(numberOne,numberTwo);
+
+		assertThat(result).isEqualTo(expected);
+
+
+
 	}
+
+	class Calculate{
+		public int add(int x,int y){
+
+			return x+y;
+		}
+	}
+
+
 
 }
